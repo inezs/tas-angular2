e603f3ab684aedd7bf43ff5921302659a6b25571
Yuliawan Rizka Syafaat <yuliawanrizka.syafaat@mitrais.com>
2017-09-04 11:37:50 +0800

    Paginator works, Data padding still need to be changed

--- src/app/home/period/period.component.ts
+++ src/app/home/period/period.component.ts
@@ -9,15 +9,22 @@ import 'rxjs/add/observable/merge';
 import 'rxjs/add/operator/map';
 
 
-
+//Structure
 @Component({
   templateUrl: './period.component.html',
   styleUrls: ['./period.component.css']
 })
 export class PeriodComponent {
+  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+  periodDatabase = new PeriodDatabase();
+  dataSource: PeriodDataSource | null;
 
+  @ViewChild(MdPaginator) paginator: MdPaginator;
   
-
+  ngOnInit() {
+    this.dataSource = new PeriodDataSource(this.periodDatabase, this.paginator);
+  }
+  
   trainingName: string;
   startDate: string;
   endDate: string;
@@ -40,6 +47,7 @@ export class PeriodComponent {
 
 }
 
+//Add Period dialog
 @Component({
   templateUrl: 'add-period-dialog.html',
   styleUrls: ['./period.component.css']
@@ -57,4 +65,66 @@ export class AddPeriodDialog {
     this.dialogRef.close();
   }
 
+}
+
+//table
+export interface Period {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+export class PeriodDatabase {
+  dataChange: BehaviorSubject<Period[]> = new BehaviorSubject<Period[]>([]);
+  get data(): Period[] { return this.dataChange.value; }
+
+  constructor() {
+    const periodData: Period[] = [
+      {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+      {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+      {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+      {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+      {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+      {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+      {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+      {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+      {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+      {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+      {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
+      {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
+      {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
+      {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
+      {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
+      {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
+      {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
+      {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
+      {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
+      {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+    ];
+    this.dataChange.next(periodData);
+  }
+}
+
+export class PeriodDataSource extends DataSource<any> {
+
+  constructor(private _periodDatabase: PeriodDatabase, private _paginator: MdPaginator) {
+    super();
+  }
+  connect(): Observable<Period[]> {
+    const displayDataChanges = [
+      this._periodDatabase.dataChange,
+      this._paginator.page,
+    ];
+
+    return Observable.merge(...displayDataChanges).map(() => {
+      const data = this._periodDatabase.data.slice();
+
+      // Grab the page's slice of data.
+      const startIndex = this._paginator.pageIndex * this._paginator.pageSize;
+      return data.splice(startIndex, this._paginator.pageSize);
+    });
+  }
+
+  disconnect() {}
 }
