6555d6797588c927f58e887418972b0f8174c997
Billy <billychartanto@gmail.com>
2017-09-05 16:49:03 +0800

    table sort

--- src/app/home/period/period.component.ts
+++ src/app/home/period/period.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, Inject, ViewChild } from '@angular/core';
-import { MdDialog, MdDialogRef, MD_DIALOG_DATA, MdPaginator } from '@angular/material';
+import { MdDialog, MdDialogRef, MD_DIALOG_DATA, MdPaginator,MdSort } from '@angular/material';
 import { FormControl, Validators } from '@angular/forms';
 import { DataSource } from '@angular/cdk/collections';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
@@ -18,15 +18,15 @@ import 'rxjs/add/observable/fromEvent';
   styleUrls: ['./period.component.css']
 })
 export class PeriodComponent {
-  displayedColumns = ['trainingName', 'activeStatus', 'coursesCount', 'startDate', 'endDate', 'createdBy', 'editedBy', 'action'];
+  displayedColumns = ['id','trainingName', 'activeStatus', 'coursesCount', 'startDate', 'endDate', 'createdBy', 'editedBy', 'action'];
   periodDatabase = new PeriodDatabase();
   dataSource: PeriodDataSource | null;
 
   @ViewChild(MdPaginator) paginator: MdPaginator;
   @ViewChild('filter') filter: ElementRef;
-  
+  @ViewChild(MdSort) sort: MdSort;
   ngOnInit() {
-    this.dataSource = new PeriodDataSource(this.periodDatabase, this.paginator);
+    this.dataSource = new PeriodDataSource(this.periodDatabase, this.paginator, this.sort);
     Observable.fromEvent(this.filter.nativeElement, 'keyup')
         .debounceTime(150)
         .distinctUntilChanged()
@@ -80,6 +80,7 @@ export class AddPeriodDialog {
 
 //table
 export interface Period {
+  id:number;
   trainingName: string;
   activeStatus: boolean;
   coursesCount: number;
@@ -95,10 +96,19 @@ export class PeriodDatabase {
 
   constructor() {
     this.dataChange.next([
-      {trainingName: 'ASP .NET', activeStatus: true , coursesCount: 3 , startDate:'10-11-2017', endDate:'20-10-2017', createdBy:"Yuliawan Rizka", editedBy: "Yuliawan Rizka"},
-      {trainingName: 'ASP .NET', activeStatus: false , coursesCount: 3 , startDate:'10-11-2017', endDate:'20-10-2017', createdBy:"Yuliawan Rizka", editedBy: "Lynx"},
-      {trainingName: 'Angular 2', activeStatus: true , coursesCount: 3 , startDate:'10-11-2017', endDate:'20-10-2017', createdBy:"Lynx", editedBy: "Lynx"},
-      {trainingName: 'NodeJS', activeStatus: false , coursesCount: 8 , startDate:'10-11-2016', endDate:'20-10-2016', createdBy:"Yuliawan Rizka", editedBy: "Yuliawan Rizka"}
+      {id: 1,trainingName: 'ASP .NET', activeStatus: true , coursesCount: 3 , startDate:'10-11-2017', endDate:'20-10-2017', createdBy:"Yuliawan Rizka", editedBy: "Yuliawan Rizka"},
+      {id: 2,trainingName: 'ASP .NET', activeStatus: false , coursesCount: 3 , startDate:'10-11-2017', endDate:'20-10-2017', createdBy:"Yuliawan Rizka", editedBy: "Lynx"},
+      {id: 3,trainingName: 'Angular 2', activeStatus: true , coursesCount: 3 , startDate:'10-11-2017', endDate:'20-10-2017', createdBy:"Lynx", editedBy: "Lynx"},
+      {id: 4,trainingName: 'NodeJS', activeStatus: false , coursesCount: 8 , startDate:'10-11-2016', endDate:'20-10-2016', createdBy:"Yuliawan Rizka", editedBy: "Yuliawan Rizka"},
+         {id: 5,trainingName: 'ASP .NET', activeStatus: true , coursesCount: 3 , startDate:'10-11-2017', endDate:'20-10-2017', createdBy:"Yuliawan Rizka", editedBy: "Yuliawan Rizka"},
+      {id: 6,trainingName: 'ASP .NET', activeStatus: false , coursesCount: 3 , startDate:'10-11-2017', endDate:'20-10-2017', createdBy:"Yuliawan Rizka", editedBy: "Lynx"},
+      {id: 7,trainingName: 'Angular 2', activeStatus: true , coursesCount: 3 , startDate:'10-11-2017', endDate:'20-10-2017', createdBy:"Lynx", editedBy: "Lynx"},
+      {id: 8,trainingName: 'NodeJS', activeStatus: false , coursesCount: 8 , startDate:'10-11-2016', endDate:'20-10-2016', createdBy:"Yuliawan Rizka", editedBy: "Yuliawan Rizka"},
+      {id: 9,trainingName: 'ASP .NET', activeStatus: true , coursesCount: 3 , startDate:'10-11-2017', endDate:'20-10-2017', createdBy:"Yuliawan Rizka", editedBy: "Yuliawan Rizka"},
+      {id: 10,trainingName: 'ASP .NET', activeStatus: false , coursesCount: 3 , startDate:'10-11-2017', endDate:'20-10-2017', createdBy:"Yuliawan Rizka", editedBy: "Lynx"},
+      {id: 11,trainingName: 'Angular 2', activeStatus: true , coursesCount: 3 , startDate:'10-11-2017', endDate:'20-10-2017', createdBy:"Lynx", editedBy: "Lynx"},
+      {id: 12,trainingName: 'NodeJS', activeStatus: false , coursesCount: 8 , startDate:'10-11-2016', endDate:'20-10-2016', createdBy:"Yuliawan Rizka", editedBy: "Yuliawan Rizka"}
+      
     ]);
   }
 }
@@ -111,7 +121,8 @@ export class PeriodDataSource extends DataSource<any> {
   }
   filteredData: Period[] = [];
   renderedData: Period[] = [];
-  constructor(private _periodDatabase: PeriodDatabase, private _paginator: MdPaginator) {
+  sortedData: Period[] = [];
+  constructor(private _periodDatabase: PeriodDatabase, private _paginator: MdPaginator, private _sort: MdSort) {
     super();
   }
   connect(): Observable<Period[]> {
@@ -119,6 +130,7 @@ export class PeriodDataSource extends DataSource<any> {
       this._periodDatabase.dataChange,
       this._filterChange,
       this._paginator.page,
+      this._sort.mdSortChange,
     ];
 
 
@@ -133,8 +145,32 @@ export class PeriodDataSource extends DataSource<any> {
       const startIndex = this._paginator.pageIndex * this._paginator.pageSize;
       this.renderedData = this.filteredData.splice(startIndex, this._paginator.pageSize);
       return this.renderedData;
+
+      //return this.getSortedData();
+
     });
   }
 
   disconnect() {}
+
+  getSortedData(): Period[] {
+    const data = this._periodDatabase.data.slice();
+    if (!this._sort.active || this._sort.direction == '') { return data; }
+
+    return data.sort((a, b) => {
+      let propertyA: number|string = '';
+      let propertyB: number|string = '';
+
+      switch (this._sort.active) {
+        case 'id': [propertyA, propertyB] = [a.id, b.id]; break;
+      }
+
+      let valueA = isNaN(+propertyA) ? propertyA : +propertyA;
+      let valueB = isNaN(+propertyB) ? propertyB : +propertyB;
+
+      return (valueA < valueB ? -1 : 1) * (this._sort.direction == 'asc' ? 1 : -1);
+    });
+  }
 }
+
+
