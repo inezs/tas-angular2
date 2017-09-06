4b2bd2042acdf288ca5fb4d556f95df1c07a470c
Billy <billychartanto@gmail.com>
2017-09-06 11:13:48 +0800

    edit popup

--- src/app/home/period/period.component.ts
+++ src/app/home/period/period.component.ts
@@ -37,16 +37,30 @@ export class PeriodComponent {
   }
   
   trainingName: string;
-  startDate: string;
+  startDate: Date;
   endDate: string;
   
 
   constructor(public addPeriod: MdDialog) {}
 
-  openDialog(): void {
+  editDialog(operation:string, period:Period)
+  {
     let dialogRef = this.addPeriod.open(AddPeriodDialog, {
       width: '40%',
-      data: { trainingName: "" }
+      data: { trainingName: period.trainingName, startDate: new Date(period.startDate), endDate:new Date(period.endDate) ,operation: operation }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.trainingName = result.trainingName;
+      this.startDate = result.startDate;
+      this.endDate = result.endDate;
+    });
+  }
+  openDialog(operation:string): void {
+  
+    let dialogRef = this.addPeriod.open(AddPeriodDialog, {
+      width: '40%',
+      data: { trainingName: "",operation: operation }
     });
 
     dialogRef.afterClosed().subscribe(result => {
@@ -58,7 +72,7 @@ export class PeriodComponent {
 
 }
 
-//Add Period dialog
+//Add Period Component dialog
 @Component({
   templateUrl: 'add-period-dialog.html',
   styleUrls: ['./period.component.css']
@@ -96,18 +110,18 @@ export class PeriodDatabase {
 
   constructor() {
     this.dataChange.next([
-      {id: 1,trainingName: 'ASP .NET', activeStatus: true , coursesCount: 3 , startDate:'10-11-2017', endDate:'20-10-2017', createdBy:"Yuliawan Rizka", editedBy: "Yuliawan Rizka"},
-      {id: 2,trainingName: 'ASP .NET', activeStatus: false , coursesCount: 3 , startDate:'10-11-2017', endDate:'20-10-2017', createdBy:"Yuliawan Rizka", editedBy: "Lynx"},
-      {id: 3,trainingName: 'Angular 2', activeStatus: true , coursesCount: 3 , startDate:'10-11-2017', endDate:'20-10-2017', createdBy:"Lynx", editedBy: "Lynx"},
-      {id: 4,trainingName: 'NodeJS', activeStatus: false , coursesCount: 8 , startDate:'10-11-2016', endDate:'20-10-2016', createdBy:"Yuliawan Rizka", editedBy: "Yuliawan Rizka"},
-         {id: 5,trainingName: 'ASP .NET', activeStatus: true , coursesCount: 3 , startDate:'10-11-2017', endDate:'20-10-2017', createdBy:"Yuliawan Rizka", editedBy: "Yuliawan Rizka"},
-      {id: 6,trainingName: 'ASP .NET', activeStatus: false , coursesCount: 3 , startDate:'10-11-2017', endDate:'20-10-2017', createdBy:"Yuliawan Rizka", editedBy: "Lynx"},
-      {id: 7,trainingName: 'Angular 2', activeStatus: true , coursesCount: 3 , startDate:'10-11-2017', endDate:'20-10-2017', createdBy:"Lynx", editedBy: "Lynx"},
-      {id: 8,trainingName: 'NodeJS', activeStatus: false , coursesCount: 8 , startDate:'10-11-2016', endDate:'20-10-2016', createdBy:"Yuliawan Rizka", editedBy: "Yuliawan Rizka"},
-      {id: 9,trainingName: 'ASP .NET', activeStatus: true , coursesCount: 3 , startDate:'10-11-2017', endDate:'20-10-2017', createdBy:"Yuliawan Rizka", editedBy: "Yuliawan Rizka"},
-      {id: 10,trainingName: 'ASP .NET', activeStatus: false , coursesCount: 3 , startDate:'10-11-2017', endDate:'20-10-2017', createdBy:"Yuliawan Rizka", editedBy: "Lynx"},
-      {id: 11,trainingName: 'Angular 2', activeStatus: true , coursesCount: 3 , startDate:'10-11-2017', endDate:'20-10-2017', createdBy:"Lynx", editedBy: "Lynx"},
-      {id: 12,trainingName: 'NodeJS', activeStatus: false , coursesCount: 8 , startDate:'10-11-2016', endDate:'20-10-2016', createdBy:"Yuliawan Rizka", editedBy: "Yuliawan Rizka"}
+      {id: 1,trainingName: 'Training #1', activeStatus: true , coursesCount: 3 , startDate:'1-11-2017', endDate:'3-12-2017', createdBy:"Trainer #1", editedBy: "Trainer #12"},
+      {id: 2,trainingName: 'Training #2', activeStatus: false , coursesCount: 3 , startDate:'2-11-2017', endDate:'4-12-2017', createdBy:"Trainer #2", editedBy: "Trainer #11"},
+      {id: 3,trainingName: 'Training #3', activeStatus: true , coursesCount: 3 , startDate:'3-11-2017', endDate:'5-12-2017', createdBy:"Trainer #3", editedBy: "Trainer #10"},
+      {id: 4,trainingName: 'Training #4', activeStatus: false , coursesCount: 8 , startDate:'4-11-2016', endDate:'6-12-2016', createdBy:"Trainer #4", editedBy: "Trainer #9"},
+         {id: 5,trainingName: 'Training #5', activeStatus: true , coursesCount: 3 , startDate:'5-11-2017', endDate:'7-12-2017', createdBy:"Trainer #5", editedBy: "Trainer #8"},
+      {id: 6,trainingName: 'Training #6', activeStatus: false , coursesCount: 3 , startDate:'6-11-2017', endDate:'8-12-2017', createdBy:"Trainer #6", editedBy: "Trainer #7"},
+      {id: 7,trainingName: 'Training #7', activeStatus: true , coursesCount: 3 , startDate:'7-11-2017', endDate:'9-12-2017', createdBy:"Trainer #7", editedBy: "Trainer #6"},
+      {id: 8,trainingName: 'Training #8', activeStatus: false , coursesCount: 8 , startDate:'8-11-2016', endDate:'10-12-2016', createdBy:"Trainer #8", editedBy: "Trainer #5"},
+      {id: 9,trainingName: 'Training #9', activeStatus: true , coursesCount: 3 , startDate:'9-11-2017', endDate:'11-12-2017', createdBy:"Trainer #9", editedBy: "Trainer #4"},
+      {id: 10,trainingName: 'Training #10', activeStatus: false , coursesCount: 3 , startDate:'10-11-2017', endDate:'12-12-2017', createdBy:"Trainer #10", editedBy: "Trainer #3"},
+      {id: 11,trainingName: 'Training #11', activeStatus: true , coursesCount: 3 , startDate:'11-11-2017', endDate:'13-12-2017', createdBy:"Trainer #11", editedBy: "Trainer #2"},
+      {id: 12,trainingName: 'Training #12', activeStatus: false , coursesCount: 8 , startDate:'12-11-2016', endDate:'14-12-2016', createdBy:"Trainer #12", editedBy: "Trainer #1"}
       
     ]);
   }
